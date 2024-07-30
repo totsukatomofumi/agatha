837f5589e08b60feb7386ca28348203870286dcc
totsukatomofumi <totsukatomofumi@gmail.com>
2024-07-31 01:51:09 +0800

    Add start and transition to body

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState, useRef } from "react";
+
+import Start from "./Start";
 import Body from "./Body";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function App() {
-  return (
-    <div>
-      <Body />
-    </div>
+  const displayRef = useRef();
+
+  const [isStartEnd, setIsStartEnd] = useState(false);
+
+  const [display, setDisplay] = useState(
+    <Start setIsStartEnd={setIsStartEnd} />
   );
+
+  useGSAP(
+    () => {
+      if (isStartEnd) {
+        const tl = gsap.timeline();
+
+        tl.to(displayRef.current, { duration: 0.2, opacity: 0 })
+          .call(() => {
+            setDisplay(<Body />);
+          })
+          .to(displayRef.current, { duration: 0.2, opacity: 1 });
+      }
+    },
+    { dependencies: [isStartEnd] }
+  );
+
+  return <div ref={displayRef}>{display}</div>;
 }
 
 export default App;
---
+++ src/Start.jsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+function Start({ setIsStartEnd }) {
+  const clickMeRef = useRef();
+  const [isClickMeHover, setIsClickMeHover] = useState(false);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+
+      tl.to(clickMeRef.current, {
+        duration: 0.5,
+        scale: 1.2,
+        ease: "power1.inOut",
+        yoyo: true,
+        repeat: isClickMeHover ? 0 : -1,
+      });
+    },
+    { dependencies: [isClickMeHover], revertOnUpdate: true }
+  );
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
+      <button
+        ref={clickMeRef}
+        className="text-5xl hover:text-green-400"
+        onMouseEnter={() => {
+          setIsClickMeHover(true);
+        }}
+        onMouseOut={() => {
+          setIsClickMeHover(false);
+        }}
+        onMouseDown={() => {
+          setIsStartEnd(true);
+        }}
+      >
+        click me! 🐱
+      </button>
+    </div>
+  );
+}
+
+export default Start;
